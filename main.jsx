5a687b97f28ca2e56440c133b531ea144dc56360
root <root@vmi2619091.contaboserver.net>
2025-07-14 18:10:29 +0200

    chore: initial commit do frontend

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => (
+  <h1>🚀 Frontend funcionando perfeitamente 🔥</h1>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
